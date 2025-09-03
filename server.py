7b8076d889db703190ba5400dd2960aac42caf94
nkraj7266 <nkraj7266@gmail.com>
2025-09-03 10:15:04 +0530

    making main sepaprate from prod

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from logging_config import app_logger, error_logger
 from flask import Flask
 from flask_cors import CORS
